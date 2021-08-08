b72a3779500e1456f5ceeb2fb0dd8cf711d23f00
LEE HYO JEONG <hyojeong1913@naver.com>
2021-08-08 23:38:48 +0900

    AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -23,7 +25,7 @@ public class AppConfig {
      * @return
      */
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     /**
@@ -32,6 +34,28 @@ public class AppConfig {
      * @return
      */
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    /**
+     * 회원 저장소 역할
+     *
+     * new MemoryMemberRepository() 부분 중복 제거
+     * 장점 : MemoryMemberRepository 를 다른 구현체로 변경할 때 한 부분만 변경하면 된다.
+     * @return
+     */
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    /**
+     * 할인 정책 역할
+     *
+     * new FixDiscountPolicy() 부분 중복 제거
+     * 장점 : FixDiscountPolicy 를 다른 구현체로 변경할 때 한 부분만 변경하면 된다.
+     * @return
+     */
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
